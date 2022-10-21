fddaf80941096bd0f66215ab5579298ce2b2d1f8
tallespereira <tallespereira55@gmail.com>
2022-10-21 18:29:54 -0300

    Exercicios dia01

---
+++ aulas-javascript/script.js
@@ -0,0 +1,22 @@
+let studantCandidate = 'reprovado'
+
+switch (studantCandidate){
+
+case "aprovado":
+    console.log("Parabés, você foi aprovada(o)!");
+     break;
+
+case "lista":
+    console.log("Você está na nossa listá de espera.")
+     break;
+   
+case "reprovado":
+     console.log("Você foi reprovado(a).")   
+      break;
+      
+ default:
+    console.log("informação incorreta")     
+    
+  }
+
+
