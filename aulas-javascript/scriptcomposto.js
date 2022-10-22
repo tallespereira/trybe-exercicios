c7a96b72dfffff4325240c3a98410b41fae05be0
tallespereira <tallespereira55@gmail.com>
2022-10-22 16:40:17 -0300

    Primeiro bloco de exercicios do dia 02

---
+++ aulas-javascript/scriptcomposto.js
@@ -0,0 +1,94 @@
+function dice (){
+    return Math.ceil(math.random() * 6)
+}
+
+let d1 = dice();
+
+console.log(d1);
+
+
+
+
+
+
+
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+// for(let name of names){
+//     console.log(name);
+// }
+
+
+
+
+
+
+
+
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+// for(index = 0; index < groceryList.length; index++){
+//     console.log(groceryList[index]);
+
+// }
+
+
+
+
+
+
+
+
+// let numero = 982;
+
+// for(let contador = 1; contador <= 9; contador++){
+//     console.log(numero * contador);
+//     console.log('-');
+// }
+
+// let listaDeNomes =['Julia', 'Nicoli', 'Luciano'];
+
+// for (let index = 0; index < listaDeNomes.length; index++){
+//     console.log('Boas Vindas ao curso da Trybe, ' + listaDeNomes[index])
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let menu = ['Home', 'Serviços', 'Portifolio', 'Links']
+
+// let menuServices = menu[1];
+// console.log(menuServices);
+
+// let indexOfPortfolio = menu[2];
+// console.log(indexOfPortfolio);
+
+
+
+
+// menu.push('Contato');
+// // console.log(menu);
+
+
+
